a23d3a089330a32d55a8c662fd17173376f96f95
Garv Singal <178637667+Grvsingl@users.noreply.github.com>
2026-07-20 13:14:04 +0530

    Time: 49 ms (5.79%), Space: 24 MB (8.09%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i=0;
-        for(int j=0;j<nums.size();j++){
-            if(nums[j]!=0){
-                swap(nums[i],nums[j]);
-                i++;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                for(int j=i+1;j<nums.size();j++){
+                    if(nums[j]!=0){
+                        swap(nums[i],nums[j]);
+                        break;
+                    }
+                }
             }
-        }    
+        }
     }
 };
