03031366d0e0fa2b03f9b0d2e1deadc06609e1de
Garv Singal <178637667+Grvsingl@users.noreply.github.com>
2026-07-21 12:34:15 +0530

    Time: 0 ms (100%), Space: 50.3 MB (34.25%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
-        int max,count=0;
+        int maxx=0,count=0;
         for(int i=0; i<nums.size() ;i++){
             if (nums[i]!=1){
-                max= max>count?max:count;
+                maxx= max(count,maxx);
                 count=0;
             } else {
                 count++;
             }
         }
-        return max>count?max:count;
+        return max(maxx,count);
     }
 };
